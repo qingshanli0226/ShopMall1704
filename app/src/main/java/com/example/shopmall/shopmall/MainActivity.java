a096f67da0166a087840c2590486235e4e2f8a58
liqingshan <liqingshan@sina.cn>
2019-11-30 10:36:14 +0800

    开发分支功能开发ok

--- app/src/main/java/com/example/shopmall/shopmall/MainActivity.java
+++ app/src/main/java/com/example/shopmall/shopmall/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.shopmall.shopmall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //测试开发分支
         setContentView(R.layout.activity_main);
+        Log.d("LQS", "MainActivity");
     }
 }
