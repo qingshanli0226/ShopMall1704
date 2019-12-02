390411fb23972df2af9155e8371c083becb1610d
liqingshan <liqingshan@sina.cn>
2019-12-02 11:42:33 +0800

    add the 1704 log.

--- app/src/main/java/com/example/shopmall/shopmall/MainActivity.java
+++ app/src/main/java/com/example/shopmall/shopmall/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         //测试开发分支
         setContentView(R.layout.activity_main);
         Log.d("LQS", "MainActivity");
+
+        Log.d("LQS", "1704");
     }
 }
