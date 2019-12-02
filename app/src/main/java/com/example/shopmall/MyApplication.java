30d09b18a3069d3709588812660d734df20a6033
songpengjie <1102388566@qq.com>
2019-12-02 08:59:23 +0800

    Welcome

---
+++ app/src/main/java/com/example/shopmall/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.shopmall;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+
+    }
+}
