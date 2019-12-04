17a109a13f2fbb9c0417c2272373bdb77578e0c8
Your Name <you@example.com>
2019-12-04 13:52:54 +0800

    new Pro

--- app/src/main/java/com/example/administrator/shaomall/MainActivity.java
+++ app/src/main/java/com/example/administrator/shaomall/MainActivity.java
@@ -10,9 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试
-        //sas
-        Intent intent=new Intent(this, com.example.remindsteporgan.MainActivity.class);
 
     }
 }
