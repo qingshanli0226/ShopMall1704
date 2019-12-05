53fef877d21b7cbf6722b27ee9019aeb8576b3c8
songpengjie <1102388566@qq.com>
2019-12-05 08:59:29 +0800

    class

--- app/src/main/java/com/example/shopmall/fragment/ClassifyFragment.java
+++ app/src/main/java/com/example/shopmall/fragment/ClassifyFragment.java
@@ -3,10 +3,12 @@ package com.example.shopmall.fragment;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import com.example.base.BaseFragment;
 import com.example.common.TitleBar;
@@ -20,6 +22,10 @@ import java.util.ArrayList;
 public class ClassifyFragment extends BaseFragment {
 
     TitleBar tb_classify;
+    ListView lv_left;
+    RecyclerView rv_right;
+
+    private boolean isFirst = true;
 
     @Override
     protected void initData() {
@@ -43,11 +49,18 @@ public class ClassifyFragment extends BaseFragment {
             }
         });
 
+        if (isFirst){
+            
+        }
 
     }
     @Override
     protected void initView(View view) {
         tb_classify = view.findViewById(R.id.tb_classify);;
+
+        lv_left = view.findViewById(R.id.lv_left);
+        rv_right = view.findViewById(R.id.rv_right);
+
     }
 
     @Override
